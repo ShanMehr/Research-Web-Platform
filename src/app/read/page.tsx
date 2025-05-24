25cde1b9cab16919e10e0da03bfa6d21c57735e0
ShanMehr <ishanmeher25@gmail.com>
2025-05-23 17:03:10 -0700

    rendering pdfs

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Chatbot from "@/components/chatbot/Chatbot";
+import {Reader} from "@/components/reader/Reader";
 
 export default function Read() {
   return (
     <div className="flex flex-col gap-4 items-center">
+      <Reader />
       <Chatbot />
     </div>
   );
--- src/components/chatbot/ChatInput.tsx
+++
@@ -1,9 +0,0 @@
-export default function ChatInput() {
-  return (
-    <div className="flex flex-col gap-2 items-center">
-      <div className="w-full rounded-md bg-background px-4 py-2 text-sm font-semibold text-foreground">
-        Write
-      </div>
-    </div>
-  );
-}
