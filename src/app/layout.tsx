aec53673c9512b86a65d4a813f7cd9db2dd09254
ShanMehr <ishanmeher25@gmail.com>
2025-05-30 14:06:20 -0700

    AppSidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,38 +53,15 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <SidebarProvider className="bg-red-500">
+          <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
               <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                 <div className="flex items-center gap-2 px-4">
-                  <SidebarTrigger className="-ml-1" />
-                  <Separator
-                    orientation="vertical"
-                    className="mr-2 data-[orientation=vertical]:h-4"
-                  />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">
-                          Building Your Application
-                        </BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className="hidden md:block" />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
                 </div>
               </header>
               <main className="flex h-full w-full flex-col">
-                <div className="flex h-full w-full flex-col">
-                  <div className="flex h-full w-full flex-col">
-                    {children}
-                  </div>
-                </div>
-                
+                {children}
               </main>
             </SidebarInset>
           </SidebarProvider>
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,28 @@
-import { Sidebar, SidebarContent, SidebarTrigger } from "@/components/ui/sidebar"
-import { Button } from "@/components/ui/button";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+  SidebarTrigger
+} from "@/components/ui/sidebar"
  
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar>
-      {/* <SidebarTrigger className="absolute top-2 right-2" /> */}
-      <SidebarContent></SidebarContent>
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarTrigger className="absolute top-1.5 left-2.5 aspect-square w-7 items-center justify-center rounded-md p-0 outline-hidden transition-transform focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0" />
+      <SidebarHeader>
+        <h1 className="sr-only">Sidebar</h1>
+      </SidebarHeader>
+      <SidebarContent>
+        {/* <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        {/* <NavUser user={data.user} /> */}
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
   );
 }
