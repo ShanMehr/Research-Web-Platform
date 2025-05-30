1f7ea3f48465d402e56eb4e6e4402923f85a950c
ShanMehr <ishanmeher25@gmail.com>
2025-05-30 13:26:56 -0700

    main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ import {
 import { Separator } from "@/components/ui/separator";
 
 import {
-  SidebarProvider
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger
 } from "@/components/ui/sidebar";
 
 const geistSans = Geist({
@@ -51,7 +53,41 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <SidebarProvider className="bg-red-500">
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem className="hidden md:block">
+                        <BreadcrumbLink href="#">
+                          Building Your Application
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem>
+                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+              </header>
+              <main className="flex h-full w-full flex-col">
+                <div className="flex h-full w-full flex-col">
+                  <div className="flex h-full w-full flex-col">
+                    {children}
+                  </div>
+                </div>
+                
+              </main>
+            </SidebarInset>
+          </SidebarProvider>
         </body>
       </html>
     </ClerkProvider>
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,14 +1,11 @@
 import { Sidebar, SidebarContent, SidebarTrigger } from "@/components/ui/sidebar"
 import { Button } from "@/components/ui/button";
  
-export function AppSidebar() {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar>
-      <SidebarTrigger className="absolute top-2 right-2" />
-      <SidebarContent >
-      
-        
-      </SidebarContent>
+      {/* <SidebarTrigger className="absolute top-2 right-2" /> */}
+      <SidebarContent></SidebarContent>
     </Sidebar>
-  )
+  );
 }
